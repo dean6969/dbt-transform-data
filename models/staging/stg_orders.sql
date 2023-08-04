2f5d883174445641e949d53a5d2f33211549de5a
Edward Nguyen <tngu0279@student.monash.edu>
2023-08-04 18:46:01 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode, 
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} o
+Left join {{ ref('raw_customers') }} c
+ON o.customerid = c.customerid
+left join {{ ref('raw_products') }} p
+ON o.productid = p.productid
